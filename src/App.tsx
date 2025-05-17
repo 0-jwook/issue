09518d63bd3cc0fe8c4bd2c01c16c8073f602bde
My-desiny-ae <tonnyk1130@gmail.com>
2025-05-18 00:28:42 +0900

    첫 커밋

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import styled from '@emotion/styled'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Styleddiv>
+        <StyleBlock></StyleBlock>
+        <StyleBlock></StyleBlock>
+        <StyleBlock></StyleBlock>
+        <StyleBlock></StyleBlock>
+        <StyleBlock></StyleBlock>
+        <StyleBlock></StyleBlock>
+        <StyleBlock></StyleBlock>
+      </Styleddiv>
+    </>
+  )
+
+}
+const StyleBlock = styled.div`
+  background: red;
+    width: 100px;
+    height: 100px;
+    margin: 10px;
+    
+`
+
+const Styleddiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+    
+
+  /* 작은 화면에서는 세로 정렬 */
+  @media (max-width: 420px) {
+    flex-direction: column;
+    align-items: center;
+  }
+
+  /* 중간 이상 화면에서는 가로 정렬 */
+  @media (min-width: 421px) {
+    flex-direction: row;
+    align-items: flex-start;
+  }
+  `
+
+export default App
